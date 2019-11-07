e04d781aec1e38dbe75cefae83a459feb269e608
Holger Terhoeven <h.terhoeven@rto.de>
2019-11-07 16:06:12 +0100

    Fix authorization_code grant.
    Add the 'hashed_passport'-middleware to the 'passport.authorizations.authorize'-route.

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 
 Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken')
     ->middleware(['throttle', 'hashed_passport']);
+Route::getRoutes()->getByName('passport.authorizations.authorize')->middleware('hashed_passport');
